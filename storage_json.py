9a002132e4ca74479bf0cd241d754597378d6c7f
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-31 06:17:04 -0700

    added code for list_movies method, still need to complete three more functions in the StorageJson class.

--- storage_json.py
+++ storage_json.py
@@ -1,12 +1,29 @@
 from istorage import IStorage
+import json
 
 
 class StorageJson(IStorage):
     def __init__(self, file_path):
-        pass
+        self.file_path = file_path
 
     def list_movies(self):
-        pass
+        """
+        Retrieve a list of movies from the JSON file and display their names, ratings, and release years.
+
+        Returns:
+            None
+        """
+        with open(self.file_path, 'r') as f:
+            movies = json.load(f)
+
+        number_of_movies = len(movies.keys())
+        print(f'There are {number_of_movies} movies in total!')
+        for item in movies:
+            output = (
+                f'{item}, Rating: {movies[item]["rating"]} '\
+                f'Year of Release: {movies[item]["year of release"]}'
+            )
+            print(output)
 
     def add_movie(self, title, year, rating, poster):
         pass
