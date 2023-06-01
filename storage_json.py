f8cbbd3e675ed24ffd91cc5f2f4ff3f21e829e34
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-31 19:25:51 -0700

    added code to the methods in the StorageJson class.  removed code that accessed api.

--- storage_json.py
+++ storage_json.py
@@ -8,7 +8,8 @@ class StorageJson(IStorage):
 
     def list_movies(self):
         """
-        Retrieve a list of movies from the JSON file and display their names, ratings, and release years.
+        Retrieve a list of movies from the JSON file and display their
+        names, ratings, and release years.
 
         Returns:
             None
@@ -26,10 +27,69 @@ class StorageJson(IStorage):
             print(output)
 
     def add_movie(self, title, year, rating, poster):
-        pass
+        """
+        Adds a new movie to the JSON file.
+
+        Args:
+            title (str): The title of the movie.
+            year (int): The release year of the movie.
+            rating (float): The rating of the movie.
+            poster (str): the URL or path to the movie poster image.
+
+        Returns:
+            None
+        """
+        new_obj = {
+            'rating': rating,
+            'year of release': year,
+            'poster url': poster
+            }
+
+        with open(self.file_path, 'r') as f:
+            movies = json.load(f)
+        movies[title] = new_obj
+        with open(self.file_path, 'w') as f:
+            json.dump(movies, f, indent=4)
 
     def delete_movie(self, title):
-        pass
+        """
+        Deletes a movie from the JSON file.
+
+        Args:
+            title (str): The title of the movie to delete.
+
+        Returns:
+            None
+        """
+        with open(self.file_path, 'r') as f:
+            movies = json.load(f)
+
+        movie_to_delete = input('Which movie would you like to delete? Movie name: ')
+        if movies.get(movie_to_delete) is None:
+            print('This movie is not present in the database!')
+        else:
+            del movies[movie_to_delete]
+            print(f'{movie_to_delete} successfully deleted!')
+
+        with open(self.file_path, 'w') as f:
+            json.dump(movies, f, indent=4)
 
     def update_movie(self, title, notes):
-        pass
+        """
+        Updates the notes for a movie in the JSON file.
+
+        Args:
+            title (str): The title of the movie to update.
+            notes (str): The updated notes for the movie
+        """
+        with open(self.file_path, 'r') as f:
+            movies = json.load(f)
+
+        movie_to_check = input('Enter a movie name: ')
+        if movies.get(movie_to_check) is None:
+            print('This movie is not present in the database!')
+        else:
+            movies[movie_to_check]['notes'] = notes
+
+        with open(self.file_path, 'w') as f:
+            json.dump(movies, f, indent=4)
