3538ebf9388f53b5ea4a9f052805eb0c10605605
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-03 15:10:44 -0700

    added the argparse library, now you can select to open a json or csv file from the command line.  for example python main.py data.json or python main.py data.csv.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import argparse
 import storage_json
+import storage_csv
 import movie_app
 
-storage = storage_json.StorageJson('data.json')
-movie_app = movie_app.MovieApp(storage)
+parser = argparse.ArgumentParser(description='Movie App')
+
+parser.add_argument('file', help='Path to the data file (JSON or CSV)')
+
+args = parser.parse_args()
+file_path = args.file
+
+if file_path.endswith('.json'):
+    storage = storage_json.StorageJson(file_path)
+    movie_app = movie_app.MovieApp(storage)
+elif file_path.endswith('.csv'):
+    storage = storage_csv.StorageCsv(file_path)
+    movie_app = movie_app.MovieApp(storage)
 
 
 def main():
